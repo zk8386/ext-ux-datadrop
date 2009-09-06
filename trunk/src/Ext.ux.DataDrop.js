d9adf81ce7f274dc2243f0ae3a83bfa71e297811
justarock@gmail.com <justarock@gmail.com@4865792e-989b-11de-979f-3176e26b92dd>
2009-09-06 02:46:22 +0000

    - Plugin is now a singleton, among other things
    Refactored much of the code, thanks to major help from Nigel (Animal) White

--- trunk/src/Ext.ux.DataDrop.js
+++ trunk/src/Ext.ux.DataDrop.js
@@ -1,84 +1,115 @@
 Ext.ns('Ext.ux.grid');
 /**
  * @author Shea Frederick - http://www.vinylfox.com
+ * @contributor Nigel (Animal) White
  * @class Ext.ux.grid.DataDrop
- * @extends Ext.Component
+ * @singleton
  * <p>A plugin that allows data to be dragged into a grid from spreadsheet applications (tabular data).</p>
+ * <p>Sample Usage</p>
+ * <pre><code>
+{
+	xtype: 'grid',
+	...,
+	plugins: [Ext.ux.grid.DataDrop],
+	...
+}
+ * </code></pre>
  */
-Ext.ux.grid.DataDrop = Ext.extend(Ext.Component, {
-    // private
-    init: function(cmp){
-        this.cmp = cmp;
-        this.changeValueTask = {
-            run: function(){
-                this.dataDropped({}, this.textEl);
-            },
-            interval: 250,
-            scope: this
-        };
-        this.cmp.on('render', this.onRender, this, {
-            delay: 100
-        });
-    },
-    // private
-    onRender: function(){
-        this.gridBody = Ext.get(this.cmp.body.select('.x-grid3-body').elements[0]);
-        if (this.gridBody) {
-            var el = Ext.DomHelper.insertAfter(this.gridBody, {
+Ext.ux.grid.DataDrop = (function() {
+
+//  After the GridView has been rendered, insert a static textarea after it.
+//  Ensure the scroller is sized to accomodate it.
+    function onViewRender(){
+        var v = this.view;
+        if (v.mainBody) {
+            this.textEl = Ext.DomHelper.insertAfter(v.mainBody, {
                 tag: 'textarea',
                 id: Ext.id(),
                 value: '',
                 style: {
-                    'font-size': '1px',
-					overflow: 'hidden',
+					'font-size': '1px',
+                    border: '0px none',
+                    overflow: 'hidden',
                     color: '#fff',
-                    border: 'none',
-                    height: '100px'
+                    'background-color': 'transparent',
+                    margin: 0,
+                    height: '100px',
+					cursor: 'default'
                 }
+            }, true);
+            this.textEl.setVisibilityMode(Ext.Element.DISPLAY);
+            this.textEl.on({
+				mouseover: function(){
+					Ext.TaskMgr.start(this.changeValueTask);
+				},
+				mouseout: function(){
+					Ext.TaskMgr.stop(this.changeValueTask);
+				},
+                scope: this
             });
-            this.textEl = Ext.get(el);
-            this.gridScroller = Ext.get(this.cmp.body.query('.x-grid3-scroller')[0]);
-            this.resizeDropArea();
-            this.textEl.on('change', this.dataDropped, this);
-            this.textEl.on('focus', this.blur);
-            this.cmp.on('resize', this.resizeDropArea, this);
-            Ext.TaskMgr.start(this.changeValueTask);
-        }
-    },
-    // private
-    resizeDropArea: function(){
-        var wh = this.gridScroller.getSize(), scrollOffset = this.cmp.getView().scrollOffset;
-        wh.width = wh.width - scrollOffset;
-        wh.height = wh.height - this.gridBody.getHeight() - scrollOffset;
-        if (wh.height < 100) {
-            wh.height = 100;
-        }
-        this.textEl.setSize(wh);
-    },
-    // private
-    dataDropped: function(e, el){
-        var nv = el.getValue();
-        el.blur();
-        if (nv !== '') {
-            var store = this.cmp.getStore(), rec = store.reader.recordType;
-            el.dom.value = '';
-            this.nv = nv;
-            Ext.TaskMgr.stop(this.changeValueTask);
-            this.rows = this.nv.split(/\r\n|\r|\n/);
-			this.cols = this.cmp.getColumnModel().getColumnsBy(function(c){ return !c.hidden; });
-			if (this.cols.length && this.rows.length) {
-				for (i = 0; i < this.rows.length; i++) {
-					var vals = this.rows[i].split(/\s*\t\s*/), data = {}, f = 0;
-					if (vals.join('').replace(' ', '') != '') {
-						for (k = 0; k < vals.length; k++) {
-							data[this.cols[k].dataIndex] = vals[k];
-						}
-						store.add(new rec(data));
-					}
-				}
-				this.resizeDropArea();
-			}
-            Ext.TaskMgr.start(this.changeValueTask);
+			resizeDropArea.call(this);
         }
     }
-});
+
+//  on GridPanel resize, keep scroller height correct to accomodate textarea.
+    function resizeDropArea(){
+        if (this.textEl) {
+			var nh = (this.view.scroller.getHeight() - this.view.mainBody.getHeight() - 6);
+			var nw = (this.view.mainBody.getWidth());
+			if (nh < 100) { nh = 100; }
+            this.textEl.setSize({
+				height: nh,
+				width: nw
+			});
+        }
+    }
+
+//  on change of data in textarea, create a Record from the tab-delimited contents.
+    function dataDropped(e, el){
+        var nv = el.value;
+        el.blur();
+        if (nv !== '') {
+            var store = this.getStore(), Record = store.recordType;
+            el.value = '';
+            var rows = nv.split(/\r\n|\r|\n/),
+                cols = this.getColumnModel().getColumnsBy(function(c){
+                    return !c.hidden;
+                }),
+                fields = Record.prototype.fields;
+            if (cols.length && rows.length) {
+                for (var i = 0; i < rows.length; i++) {
+                    var vals = rows[i].split(/\s*\t\s*/), data = {};
+                    if (vals.join('').replace(' ', '') !== '') {
+                        for (var k = 0; k < vals.length; k++) {
+                            var fldName = cols[k].dataIndex;
+                            var fld = fields.item(fldName);
+                            data[fldName] = fld ? fld.convert(vals[k]) : vals[k];
+                        }
+                        var newRec = new Record(data);
+                        store.add(newRec);
+                        var idx = store.indexOf(newRec);
+                        this.view.focusRow(idx);
+                        Ext.get(this.view.getRow(idx)).highlight();
+                    }
+                }
+				resizeDropArea.call(this);
+            }
+        }
+    }
+
+    return {
+        init: function(cmp){
+            Ext.apply(cmp, {
+                changeValueTask: {
+                    run: function(){
+                        dataDropped.call(this, null, this.textEl.dom);
+                    },
+                    interval: 100,
+                    scope: cmp
+                },
+                onResize: cmp.onResize.createSequence(resizeDropArea)
+            });
+            cmp.getView().afterRender = cmp.getView().afterRender.createSequence(onViewRender, cmp);
+        }
+    };
+})();
