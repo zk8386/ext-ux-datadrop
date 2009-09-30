1dd6b2ab7a4bcb3be15481e3f446b1e4d947c2bc
justarock@gmail.com <justarock@gmail.com@4865792e-989b-11de-979f-3176e26b92dd>
2009-09-30 15:00:37 +0000

    - mouse event forwarding override added. (Thanks to Nigel (Animal)) - grid now allows data to be dropped over existing rows of data.

--- trunk/src/Ext.ux.DataDrop.js
+++ trunk/src/Ext.ux.DataDrop.js
@@ -5,6 +5,7 @@ Ext.ns('Ext.ux.grid');
  * @class Ext.ux.grid.DataDrop
  * @singleton
  * <p>A plugin that allows data to be dragged into a grid from spreadsheet applications (tabular data).</p>
+ * <p>Requires the Override.js file which adds mouse event forwarding capability to ExtJS</p>
  * <p>Sample Usage</p>
  * <pre><code>
  {
@@ -17,12 +18,14 @@ Ext.ns('Ext.ux.grid');
  */
 Ext.ux.grid.DataDrop = (function(){
 
-    //  After the GridView has been rendered, insert a static textarea after it.
-    //  Ensure the scroller is sized to accomodate it.
+    var lineEndRE = /\r\n|\r|\n/,
+        sepRe = /\s*\t\s*/;
+
+    //  After the GridView has been rendered, insert a static transparent textarea over it.
     function onViewRender(){
         var v = this.view;
         if (v.mainBody) {
-            this.textEl = Ext.DomHelper.insertAfter(v.mainBody, {
+            this.textEl = Ext.DomHelper.insertAfter(v.scroller, {
                 tag: 'textarea',
                 id: Ext.id(),
                 value: '',
@@ -31,13 +34,16 @@ Ext.ux.grid.DataDrop = (function(){
                     border: '0px none',
                     overflow: 'hidden',
                     color: '#fff',
-                    'background-color': 'transparent',
+                    position: 'absolute',
+                    top: v.mainHd.getHeight() + 'px',
+                    left: '0px',
+                    'background-color': '#fff',
                     margin: 0,
-                    height: '100px',
                     cursor: 'default'
                 }
             }, true);
-            this.textEl.setVisibilityMode(Ext.Element.DISPLAY);
+            this.textEl.setOpacity(0.1);
+            this.textEl.forwardMouseEvents();
             this.textEl.on({
                 mouseover: function(){
                     Ext.TaskMgr.start(this.changeValueTask);
@@ -54,15 +60,14 @@ Ext.ux.grid.DataDrop = (function(){
     //  on GridPanel resize, keep scroller height correct to accomodate textarea.
     function resizeDropArea(){
         if (this.textEl) {
-            var nh = (this.view.scroller.getHeight() - this.view.mainBody.getHeight() - 6);
-            var nw = (this.view.mainBody.getWidth());
-            if (nh < 100) {
-                nh = 100;
-            }
-            this.textEl.setSize({
-                height: nh,
-                width: nw
-            });
+            var v = this.view,
+                sc = v.scroller,
+                scs = sc.getSize,
+                s = {
+                    width: sc.dom.clientWidth || (scs.width - v.getScrollOffset() + 2),
+                    height: sc.dom.clientHeight || scs.height
+                };
+            this.textEl.setSize(s);
         }
     }
     
@@ -73,12 +78,12 @@ Ext.ux.grid.DataDrop = (function(){
         if (nv !== '') {
             var store = this.getStore(), Record = store.recordType;
             el.value = '';
-            var rows = nv.split(/\r\n|\r|\n/), cols = this.getColumnModel().getColumnsBy(function(c){
+            var rows = nv.split(lineEndRE), cols = this.getColumnModel().getColumnsBy(function(c){
                 return !c.hidden;
             }), fields = Record.prototype.fields;
             if (cols.length && rows.length) {
                 for (var i = 0; i < rows.length; i++) {
-                    var vals = rows[i].split(/\s*\t\s*/), data = {};
+                    var vals = rows[i].split(sepRe), data = {};
                     if (vals.join('').replace(' ', '') !== '') {
                         for (var k = 0; k < vals.length; k++) {
                             var fldName = cols[k].dataIndex;
